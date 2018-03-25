cfb0fda26312d4abea4207abb69d93e25899d0f7
Vlad Poenaru <vlad.wing@gmail.com>
2018-03-25 13:22:15 +0800

    [CARBA]: organized the code

---
+++ projects/carba-detect/config.py
@@ -0,0 +1,7 @@
+MIN_VOLTAGE = 0.0 # volts # TODO: we actually need -0.1V
+MAX_VOLTAGE = 1.3 # volts
+
+GRID_FREQ = 50 # Hertz
+
+SAWTOOTH_PERIOD = 26.0 # seconds
+SAWTOOTH_STEPS = 200 # TODO: maybe change later
