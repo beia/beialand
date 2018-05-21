e62cb4793be6ee4977bcc375e9796d5e320d0889
Dorinel Filip <dorinel.filip@gmail.com>
2018-05-21 23:58:01 +0300

    [Meteotest-Xamarin][iOS]: Webview opens in proper scale to fit on the screen
    Enabled ScrollEnabled and ScalesPageToFit on webview using an extended render definition

---
+++ projects/Meteotest-Xamarin/meteotestforecast.iOS/WebViewFitRender.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(WebView), typeof(meteotestforecast.iOS.WebViewFitRender))]
+namespace meteotestforecast.iOS
+{
+    public class WebViewFitRender : WebViewRenderer 
+    {
+        protected override void OnElementChanged(VisualElementChangedEventArgs e) {
+            base.OnElementChanged(e);
+            var view = (UIKit.UIWebView)NativeView;
+            view.ScrollView.ScrollEnabled = true;
+            view.ScalesPageToFit = true;
+        }
+    }
+
+
+}
