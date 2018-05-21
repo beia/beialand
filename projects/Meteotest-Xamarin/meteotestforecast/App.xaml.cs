ca454587bf8e42db7b0f96ec3fdfb591c506de0d
Dorinel Filip <dorinel.filip@gmail.com>
2018-05-21 20:43:16 +0300

    [Meteotest-Xamarin]: Replaced main page with a wrapping navigation page

--- projects/Meteotest-Xamarin/meteotestforecast/App.xaml.cs
+++ projects/Meteotest-Xamarin/meteotestforecast/App.xaml.cs
@@ -10,8 +10,7 @@ namespace meteotestforecast
         public App()
         {
             InitializeComponent();
-
-            MainPage = new MainPage();
+            MainPage = new NavigationPage(new MainPage());
         }
 
         protected override void OnStart()
