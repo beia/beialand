9a602ed9ee65be22d6833c82ae4a301c9c444ecb
Dorinel Filip <dorinel.filip@gmail.com>
2018-05-21 19:57:00 +0300

    [Meteotest-Xamarin]: Create city model

---
+++ projects/Meteotest-Xamarin/meteotestforecast/City.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace meteotestforecast
+{
+    public class City
+    {
+        public string Name { get; set; }
+        public string Url { get; set; }
+
+        public City()
+        {
+            
+        }
+
+        public City(String Name, String Url)
+        {
+            this.Name = Name;
+            this.Url = Url;
+        }
+
+
+    }
+}
