f0b3b3a3961b1cf5d3192a7ce7de7cbf4d7f7437
Dorinel Filip <dorinel.filip@gmail.com>
2018-05-22 00:34:52 +0300

    [Meteotest-Xamarin][Android]: Webview opens in proper scale to fit on the screen

---
+++ projects/Meteotest-Xamarin/meteotestforecast.Android/WebViewFitRender.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms.Platform.Android;
+using Xamarin.Forms;
+[assembly: ExportRenderer(typeof(WebView), typeof(meteotestforecast.Android.WebViewFitRenderer))]
+namespace meteotestforecast.Android
+{
+#pragma warning disable CS0618 // Type or member is obsolete
+    public class WebViewFitRenderer : WebViewRenderer
+    {
+        
+        protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            if (Control != null)
+            {
+
+                Control.Settings.BuiltInZoomControls = true;
+                Control.Settings.DisplayZoomControls = false;
+                Control.Settings.LoadWithOverviewMode = true;
+                Control.Settings.UseWideViewPort = true;
+            }
+            base.OnElementPropertyChanged(sender, e);
+        }
+    }
+#pragma warning restore CS0618 // Type or member is obsolete
+}
