74fb32076b130e3a77b528e801445b098e1faf43
Dorinel Filip <dorinel.filip@gmail.com>
2018-05-21 19:57:29 +0300

    [Meteotest-Xamarin]: Created Constants class with dummy locations

---
+++ projects/Meteotest-Xamarin/meteotestforecast/Constants.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace meteotestforecast
+{
+    public static class Constants
+    {
+        public static readonly City[] locations =
+        {
+            new City("Bucuresti", "http://www.google.ro"),
+            new City("Bucuresti2", "http://www.google.ro"),
+            new City("Bucuresti3", "http://www.google.ro"),
+
+        };
+
+    }
+}
