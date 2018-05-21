a0437d24c56fdc4877a923aaea4f3c36b3fc7c5b
Dorinel Filip <dorinel.filip@gmail.com>
2018-05-21 19:58:50 +0300

    [Meteotest-Xamarin]: Added selection options to Picker and attached a dummy action

--- projects/Meteotest-Xamarin/meteotestforecast/MainPage.xaml.cs
+++ projects/Meteotest-Xamarin/meteotestforecast/MainPage.xaml.cs
@@ -14,7 +14,36 @@ namespace meteotestforecast
         {
             Debug.WriteLine("Executing MainPage constructor");
             InitializeComponent();
+
+            // Set image Source
+            // TODO: Use a middleware + XML instead
             logoImage.Source = ImageSource.FromResource("meteotestforecast.sigla-beia.png");
+
+            // Add items to the picker
+            foreach (var city in Constants.locations)
+            {
+                cityPicker.Items.Add(city.Name);
+            }
+
+
+        }
+
+        async void OnCityPickerSelectedIndexChanged(object sender, EventArgs e)
+        {
+            var picker = (Picker)sender;
+            int selectedIndex = picker.SelectedIndex;
+
+            if (selectedIndex != -1)
+            {
+                await DisplayAlert("Selection",
+                                   "We found that you selected",
+                                   Constants.locations[selectedIndex].Name);
+            }
+
+            // Make sure that selecting the same element again will trigger
+            // the event.
+            cityPicker.SelectedIndex = -1;
         }
     }
+
 }
