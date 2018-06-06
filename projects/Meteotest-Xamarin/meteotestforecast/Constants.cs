603d91b99b04b7207dffc466f21fd6fe5913ce28
Dorinel Filip <dorinel.filip@gmail.com>
2018-06-06 19:12:58 +0300

    [Meteotest-Xamarin]: Meteotest changed. We can not use HTTPS

--- projects/Meteotest-Xamarin/meteotestforecast/Constants.cs
+++ projects/Meteotest-Xamarin/meteotestforecast/Constants.cs
@@ -5,13 +5,14 @@ namespace meteotestforecast
     {
         public static readonly City[] locations =
         {
-            new City("Zanesti", "https://api.meteotest.ch/beia/v1/forecast?location=1"),
-            new City("Corbi", "https://api.meteotest.ch/beia/v1/forecast?location=2"),
-            new City("Zabala", "https://api.meteotest.ch/beia/v1/forecast?location=3"),
-            new City("Caracal", "https://api.meteotest.ch/beia/v1/forecast?location=4"),
-            new City("Insuratei", "https://api.meteotest.ch/beia/v1/forecast?location=5"),
-            new City("Comlosu Mic", "https://api.meteotest.ch/beia/v1/forecast?location=6"),
-            new City("Marghita", "https://api.meteotest.ch/beia/v1/forecast?location=7"),
+            new City("Bucharest, Beia Consult Office", "http://api.meteotest.ch/beia/v1/forecast?lat=44.3959788&lon=26.1005863"),
+            new City("Zanesti", "http://api.meteotest.ch/beia/v1/forecast?location=1"),
+            new City("Corbi", "http://api.meteotest.ch/beia/v1/forecast?location=2"),
+            new City("Zabala", "http://api.meteotest.ch/beia/v1/forecast?location=3"),
+            new City("Caracal", "http://api.meteotest.ch/beia/v1/forecast?location=4"),
+            new City("Insuratei", "http://api.meteotest.ch/beia/v1/forecast?location=5"),
+            new City("Comlosu Mic", "http://api.meteotest.ch/beia/v1/forecast?location=6"),
+            new City("Marghita", "http://api.meteotest.ch/beia/v1/forecast?location=7"),
         };
 
         public static readonly string APP_NAME = "Beia Meteotest";
