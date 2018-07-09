51f461985d4f1ee1c24fdb14899f237aaf21191d
Vlad Wing <vladwing@users.noreply.github.com>
2018-07-10 00:39:32 +0800

    [GIT]: reset branch to what we expect

--- platform/scripts/git_update.sh
+++ platform/scripts/git_update.sh
@@ -4,6 +4,9 @@ ROOT_FOLDER=/srv/beialand
 GIT=${GIT:-git}
 
 cd ${ROOT_FOLDER}
+${GIT} reset --hard
+${GIT} clean -df
+${GIT} checkout -B ${BRANCH:-master} origin/${BRANCH:-master}
 ${GIT} pull --rebase
 
 make -f platform/makefiles/Makefile.server server_deploy
