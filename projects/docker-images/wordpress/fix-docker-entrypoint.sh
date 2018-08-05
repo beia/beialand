6aa0b1637e223698c7ca85c2801a3017094e786e
Vlad Wing <vladwing@users.noreply.github.com>
2018-08-05 15:25:28 +0800

    [DOCKER]: beautify code

--- projects/docker-images/wordpress/fix-docker-entrypoint.sh
+++ projects/docker-images/wordpress/fix-docker-entrypoint.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
+PHP_SMTP_CONFIG=/usr/local/etc/php/conf.d/sendmail.ini
+ENTRYPOINT_SCRIPT=/usr/local/bin/docker-entrypoint.sh
 
-echo "sendmail_path = /usr/sbin/ssmtp -t" >> /usr/local/etc/php/conf.d/sendmail.ini
-head -n -1 /usr/local/bin/docker-entrypoint.sh > /usr/local/bin/docker-entrypoint.sh.new
-cat /tmp/generate_ssmtp_conf.sh >> /usr/local/bin/docker-entrypoint.sh.new
-tail -n -1 /usr/local/bin/docker-entrypoint.sh >> /usr/local/bin/docker-entrypoint.sh.new
-mv /usr/local/bin/docker-entrypoint.sh.new /usr/local/bin/docker-entrypoint.sh
-chmod a+x /usr/local/bin/docker-entrypoint.sh
+echo "sendmail_path = /usr/sbin/ssmtp -t" >> $PHP_SMTP_CONFIG
+(
+	head -n -1 ${ENTRYPOINT_SCRIPT}
+	cat /tmp/generate_ssmtp_conf.sh
+	tail -n -1 ${ENTRYPOINT_SCRIPT}
+) > ${ENTRYPOINT_SCRIPT}.new
+mv ${ENTRYPOINT_SCRIPT}.new ${ENTRYPOINT_SCRIPT}
+chmod a+x ${ENTRYPOINT_SCRIPT}
