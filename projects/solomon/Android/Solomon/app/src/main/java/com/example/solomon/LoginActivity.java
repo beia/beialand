dcd356aaa1625f0c75bba5463fd7d4cc2a8278af
Balanean Cristian <cristianbalanean_etti@yahoo.com>
2019-03-22 01:02:56 +0200

    [SOLOMON]: Android update 3 - design update

--- projects/solomon/Android/Solomon/app/src/main/java/com/example/solomon/LoginActivity.java
+++ projects/solomon/Android/Solomon/app/src/main/java/com/example/solomon/LoginActivity.java
@@ -1,5 +1,10 @@
 package com.example.solomon;
 
+import android.content.res.ColorStateList;
+import android.graphics.PorterDuff;
+import android.support.annotation.ColorInt;
+import android.support.v4.view.ViewCompat;
+import android.text.InputType;
 import android.util.*;
 import android.content.Context;
 import android.content.res.Resources;
@@ -22,6 +27,7 @@ import com.example.solomon.networkPackets.SignInData;
 import com.example.solomon.networkPackets.SignUpData;
 import com.example.solomon.runnables.SendAuthenticationDataRunnable;
 
+import java.lang.reflect.Field;
 import java.net.*;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
@@ -48,6 +54,7 @@ public class LoginActivity extends AppCompatActivity {
     public static RadioButton signupRadioButton;
     public static TextView titleTextView;
     public static int hintTextColor = Color.argb(50, 0, 0, 0);
+    public static int orangeAccentColor = Color.argb(200,255, 161, 114);
     //sign in UI variables
     public static EditText usernameSignInEditText;
     public static EditText passwordSignInEditText;
@@ -161,8 +168,8 @@ public class LoginActivity extends AppCompatActivity {
         loginRadioButton = findViewById(R.id.LoginRadioButton);
         signupRadioButton = findViewById(R.id.SignUpRadioButton);
         titleTextView = findViewById(R.id.TitleTextView);
-        Drawable backround = ContextCompat.getDrawable(context, R.drawable.orangecoral);
-        backround.setAlpha(200);
+        Drawable backround = ContextCompat.getDrawable(context, R.drawable.backround1);
+        backround.setAlpha(180);
         mainLinearLayout.setBackground(backround);
     }
     public void setLoginLayout()
@@ -186,46 +193,33 @@ public class LoginActivity extends AppCompatActivity {
         int width = (int)(3 * px);
         int height = (int) px;
 
-        int loginButtonWidth = (int) (2 * px);
-        int loginButtonHeight = (int) (px);
+        int loginButtonWidth = (int) (3 * px);
+        int loginButtonHeight = (int) (px / 1.2f);
 
         //add login UI in the linear Layout
-
-        TextView usernameTextView = new TextView(LoginActivity.context);
-        LinearLayout.LayoutParams layoutParamsUsernameTextView = new LinearLayout.LayoutParams(loginLinearLayout.getLayoutParams().MATCH_PARENT, loginLinearLayout.getLayoutParams().WRAP_CONTENT);
-        layoutParamsUsernameTextView.setMargins(0, 100, 0, 0);
-        usernameTextView.setLayoutParams(layoutParamsUsernameTextView);
-        usernameTextView.setTextSize(15);
-        usernameTextView.setTextColor(Color.BLACK);
-        usernameTextView.setPadding(14, 14, 14, 14);
-        usernameTextView.setText("Username: ");
 
 
         LoginActivity.usernameSignInEditText = new EditText(LoginActivity.context);
         LinearLayout.LayoutParams layoutParamsUsernameEditText = new LinearLayout.LayoutParams(width , height);
-        layoutParamsUsernameEditText.setMargins(0, 10, 0, 0);
+        layoutParamsUsernameEditText.setMargins(0, 300, 0, 0);
         layoutParamsUsernameEditText.gravity = Gravity.CENTER;
         usernameSignInEditText.setLayoutParams(layoutParamsUsernameEditText);
-        usernameSignInEditText.setHint("enter username");
+        usernameSignInEditText.setHint("username");
+        setCursorColor(usernameSignInEditText, orangeAccentColor);
+        ColorStateList colorStateList = ColorStateList.valueOf(orangeAccentColor);
+        ViewCompat.setBackgroundTintList(usernameSignInEditText, colorStateList);
         usernameSignInEditText.setHintTextColor(hintTextColor);
-
-
-        TextView passwordTextView = new TextView(LoginActivity.context);
-        LinearLayout.LayoutParams layoutParamsPasswordTextView = new LinearLayout.LayoutParams(loginLinearLayout.getLayoutParams().MATCH_PARENT, loginLinearLayout.getLayoutParams().WRAP_CONTENT);
-        layoutParamsPasswordTextView.setMargins(0, 10, 0, 0);
-        passwordTextView.setLayoutParams(layoutParamsPasswordTextView);
-        passwordTextView.setTextSize(15);
-        passwordTextView.setTextColor(Color.BLACK);
-        passwordTextView.setPadding(14, 200, 14, 14);
-        passwordTextView.setText("Password: ");
 
 
         LoginActivity.passwordSignInEditText = new EditText(LoginActivity.context);
         LinearLayout.LayoutParams layoutParamsPasswordEditText = new LinearLayout.LayoutParams(width , height);
-        layoutParamsPasswordEditText.setMargins(0, 10, 0, 0);
+        layoutParamsPasswordEditText.setMargins(0, 100, 0, 0);
         layoutParamsPasswordEditText.gravity = Gravity.CENTER;
         passwordSignInEditText.setLayoutParams(layoutParamsPasswordEditText);
-        passwordSignInEditText.setHint("enter password");
+        passwordSignInEditText.setHint("password");
+        setCursorColor(passwordSignInEditText, orangeAccentColor);
+        ViewCompat.setBackgroundTintList(passwordSignInEditText, colorStateList);
+        passwordSignInEditText.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
         passwordSignInEditText.setHintTextColor(hintTextColor);
 
 
@@ -234,6 +228,7 @@ public class LoginActivity extends AppCompatActivity {
         layoutParamsLoginButton.gravity = Gravity.CENTER;
         layoutParamsLoginButton.setMargins(0, 100, 0, 0);
         signInButton.setLayoutParams(layoutParamsLoginButton);
+        signInButton.setBackgroundColor(Color.argb(100, 255, 255, 255));
         signInButton.setText("Login");
 
         //sign in button listener
@@ -245,9 +240,7 @@ public class LoginActivity extends AppCompatActivity {
         });
 
 
-        loginLinearLayout.addView(usernameTextView);
         loginLinearLayout.addView(LoginActivity.usernameSignInEditText);
-        loginLinearLayout.addView(passwordTextView);
         loginLinearLayout.addView(LoginActivity.passwordSignInEditText);
         loginLinearLayout.addView(LoginActivity.signInButton);
     }
@@ -273,8 +266,8 @@ public class LoginActivity extends AppCompatActivity {
         int width = (int)(3 * px);
         int height = (int) px;
 
-        int signUpButtonWidth = (int) (2 * px);
-        int signUpButtonHeight = (int) (px);
+        int signUpButtonWidth = (int) (3 * px);
+        int signUpButtonHeight = (int) (px / 1.2f);
 
 
         //add login UI in the linear Layout
@@ -282,7 +275,7 @@ public class LoginActivity extends AppCompatActivity {
 
         TextView lastNameTextView = new TextView(LoginActivity.context);
         LinearLayout.LayoutParams layoutParamsLastNameTextView = new LinearLayout.LayoutParams(loginLinearLayout.getLayoutParams().MATCH_PARENT, loginLinearLayout.getLayoutParams().WRAP_CONTENT);
-        layoutParamsLastNameTextView.setMargins(0, 100, 0, 0);
+        layoutParamsLastNameTextView.setMargins(100, 100, 0, 0);
         lastNameTextView.setLayoutParams(layoutParamsLastNameTextView);
         lastNameTextView.setTextSize(15);
         lastNameTextView.setTextColor(Color.BLACK);
@@ -296,12 +289,15 @@ public class LoginActivity extends AppCompatActivity {
         layoutParamsLastNameEditText.gravity = Gravity.CENTER;
         lastNameSignUpEditText.setLayoutParams(layoutParamsLastNameEditText);
         lastNameSignUpEditText.setHint("enter last name");
+        setCursorColor(lastNameSignUpEditText, orangeAccentColor);
+        ColorStateList colorStateList = ColorStateList.valueOf(orangeAccentColor);
+        ViewCompat.setBackgroundTintList(lastNameSignUpEditText, colorStateList);
         lastNameSignUpEditText.setHintTextColor(hintTextColor);
 
 
         TextView firstNameTextView = new TextView(LoginActivity.context);
         LinearLayout.LayoutParams layoutParamsFirstNameTextView = new LinearLayout.LayoutParams(loginLinearLayout.getLayoutParams().MATCH_PARENT, loginLinearLayout.getLayoutParams().WRAP_CONTENT);
-        layoutParamsFirstNameTextView.setMargins(0, 10, 0, 0);
+        layoutParamsFirstNameTextView.setMargins(100, 10, 0, 0);
         firstNameTextView.setLayoutParams(layoutParamsFirstNameTextView);
         firstNameTextView.setTextSize(15);
         firstNameTextView.setTextColor(Color.BLACK);
@@ -315,12 +311,14 @@ public class LoginActivity extends AppCompatActivity {
         layoutParamsFirstNameEditText.gravity = Gravity.CENTER;
         firstNameSignUpEditText.setLayoutParams(layoutParamsFirstNameEditText);
         firstNameSignUpEditText.setHint("enter first name");
+        setCursorColor(firstNameSignUpEditText, orangeAccentColor);
+        ViewCompat.setBackgroundTintList(firstNameSignUpEditText, colorStateList);
         firstNameSignUpEditText.setHintTextColor(hintTextColor);
 
 
         TextView ageTextView = new TextView(LoginActivity.context);
         LinearLayout.LayoutParams layoutParamsAgeTextView = new LinearLayout.LayoutParams(loginLinearLayout.getLayoutParams().MATCH_PARENT, loginLinearLayout.getLayoutParams().WRAP_CONTENT);
-        layoutParamsAgeTextView.setMargins(0, 10, 0, 0);
+        layoutParamsAgeTextView.setMargins(100, 10, 0, 0);
         ageTextView.setLayoutParams(layoutParamsAgeTextView);
         ageTextView.setTextSize(15);
         ageTextView.setTextColor(Color.BLACK);
@@ -334,12 +332,14 @@ public class LoginActivity extends AppCompatActivity {
         layoutParamsAgeEditText.gravity = Gravity.CENTER;
         ageSignUpEditText.setLayoutParams(layoutParamsAgeEditText);
         ageSignUpEditText.setHint("enter age");
+        setCursorColor(ageSignUpEditText, orangeAccentColor);
+        ViewCompat.setBackgroundTintList(ageSignUpEditText, colorStateList);
         ageSignUpEditText.setHintTextColor(hintTextColor);
 
 
         TextView usernameTextView = new TextView(LoginActivity.context);
         LinearLayout.LayoutParams layoutParamsUsernameTextView = new LinearLayout.LayoutParams(loginLinearLayout.getLayoutParams().MATCH_PARENT, loginLinearLayout.getLayoutParams().WRAP_CONTENT);
-        layoutParamsUsernameTextView.setMargins(0, 10, 0, 0);
+        layoutParamsUsernameTextView.setMargins(100, 10, 0, 0);
         usernameTextView.setLayoutParams(layoutParamsUsernameTextView);
         usernameTextView.setTextSize(15);
         usernameTextView.setTextColor(Color.BLACK);
@@ -353,12 +353,14 @@ public class LoginActivity extends AppCompatActivity {
         layoutParamsUsernameEditText.gravity = Gravity.CENTER;
         usernameSignUpEditText.setLayoutParams(layoutParamsUsernameEditText);
         usernameSignUpEditText.setHint("enter username");
+        setCursorColor(usernameSignUpEditText, orangeAccentColor);
+        ViewCompat.setBackgroundTintList(usernameSignUpEditText, colorStateList);
         usernameSignUpEditText.setHintTextColor(hintTextColor);
 
 
         TextView passwordTextView = new TextView(LoginActivity.context);
         LinearLayout.LayoutParams layoutParamsPasswordTextView = new LinearLayout.LayoutParams(loginLinearLayout.getLayoutParams().MATCH_PARENT, loginLinearLayout.getLayoutParams().WRAP_CONTENT);
-        layoutParamsPasswordTextView.setMargins(0, 10, 0, 0);
+        layoutParamsPasswordTextView.setMargins(100, 10, 0, 0);
         passwordTextView.setLayoutParams(layoutParamsPasswordTextView);
         passwordTextView.setTextSize(15);
         passwordTextView.setTextColor(Color.BLACK);
@@ -372,13 +374,16 @@ public class LoginActivity extends AppCompatActivity {
         layoutParamsPasswordEditText.gravity = Gravity.CENTER;
         passwordSignUpEditText.setLayoutParams(layoutParamsPasswordEditText);
         passwordSignUpEditText.setHint("enter password");
+        setCursorColor(passwordSignUpEditText, orangeAccentColor);
+        ViewCompat.setBackgroundTintList(passwordSignUpEditText, colorStateList);
+        passwordSignUpEditText.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
         passwordSignUpEditText.setHintTextColor(hintTextColor);
 
 
 
         TextView passwordConfirationTextView = new TextView(LoginActivity.context);
         LinearLayout.LayoutParams layoutParamsPasswordConfirmationTextView = new LinearLayout.LayoutParams(loginLinearLayout.getLayoutParams().MATCH_PARENT, loginLinearLayout.getLayoutParams().WRAP_CONTENT);
-        layoutParamsPasswordConfirmationTextView.setMargins(0, 10, 0, 0);
+        layoutParamsPasswordConfirmationTextView.setMargins(100, 10, 0, 0);
         passwordConfirationTextView.setLayoutParams(layoutParamsPasswordConfirmationTextView);
         passwordConfirationTextView.setTextSize(15);
         passwordConfirationTextView.setTextColor(Color.BLACK);
@@ -392,14 +397,18 @@ public class LoginActivity extends AppCompatActivity {
         layoutParamsPasswordConfirmationEditText.gravity = Gravity.CENTER;
         passwordConfirmationSignUpEditText.setLayoutParams(layoutParamsPasswordConfirmationEditText);
         passwordConfirmationSignUpEditText.setHint("enter password");
-        passwordConfirationTextView.setHintTextColor(hintTextColor);
+        setCursorColor(passwordConfirmationSignUpEditText, orangeAccentColor);
+        ViewCompat.setBackgroundTintList(passwordConfirmationSignUpEditText, colorStateList);
+        passwordConfirmationSignUpEditText.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+        passwordConfirmationSignUpEditText.setHintTextColor(hintTextColor);
 
 
         LoginActivity.signUpButton = new Button(LoginActivity.context);
         LinearLayout.LayoutParams layoutParamsSignUpButton = new LinearLayout.LayoutParams(signUpButtonWidth, signUpButtonHeight);
         layoutParamsSignUpButton.gravity = Gravity.CENTER;
-        layoutParamsSignUpButton.setMargins(0, 100, 0, 0);
+        layoutParamsSignUpButton.setMargins(0, 100, 0, 100);
         signUpButton.setLayoutParams(layoutParamsSignUpButton);
+        signUpButton.setBackgroundColor(Color.argb(100, 255, 255, 255));
         signUpButton.setText("Sign up");
 
         //sign up button listener
@@ -424,5 +433,33 @@ public class LoginActivity extends AppCompatActivity {
         loginLinearLayout.addView(passwordConfirationTextView);
         loginLinearLayout.addView(passwordConfirmationSignUpEditText);
         loginLinearLayout.addView(signUpButton);
+    }
+
+
+
+    //change cursor color
+    public static void setCursorColor(EditText view, @ColorInt int color) {
+        try {
+            // Get the cursor resource id
+            Field field = TextView.class.getDeclaredField("mCursorDrawableRes");
+            field.setAccessible(true);
+            int drawableResId = field.getInt(view);
+
+            // Get the editor
+            field = TextView.class.getDeclaredField("mEditor");
+            field.setAccessible(true);
+            Object editor = field.get(view);
+
+            // Get the drawable and set a color filter
+            Drawable drawable = ContextCompat.getDrawable(view.getContext(), drawableResId);
+            drawable.setColorFilter(color, PorterDuff.Mode.SRC_IN);
+            Drawable[] drawables = {drawable, drawable};
+
+            // Set the drawables
+            field = editor.getClass().getDeclaredField("mCursorDrawable");
+            field.setAccessible(true);
+            field.set(editor, drawables);
+        } catch (Exception ignored) {
+        }
     }
 }
