ebdbbdc3ccc3c955a7106577b17183e2de4ccec3
Balanean Cristian <cristianbalanean_etti@yahoo.com>
2019-07-19 18:58:45 +0300

    [SOLOMON]: Update 2-22 Python ml contour finding algorithm for stores

--- projects/solomon/ImageProcessing/ImageProcessing.py
+++ projects/solomon/ImageProcessing/ImageProcessing.py
@@ -1,6 +1,7 @@
 import cv2
 import random
 import matplotlib.pyplot as plt
+import numpy as np
 
 #get the image from the memory and convert to grayscale
 path = r"C:\Users\beia\Desktop\StoreMaps\map6.jpg"
@@ -12,8 +13,16 @@ try:
 except IOError:
     pass
 
+#remove the black pixel from the image and replace them with the backround
+'''
+for i in range(grayScaleImage.shape[0]):
+    for j in range(grayScaleImage.shape[1]):
+        if originalImage[i][j] == 0:
+'''
+
+
 #mean blur - for testing purposes
-meanBluredImage = cv2.blur(grayScaleImage, (9, 9))
+meanBluredImage = cv2.blur(grayScaleImage, (3, 3))
 #gaussian blur - for testing purposes
 gaussianBluredImage = cv2.GaussianBlur(grayScaleImage,(3,3),0)
 
@@ -22,19 +31,41 @@ sobelx = cv2.Sobel(gaussianBluredImage,cv2.CV_64F,1,0,ksize=3)
 sobely = cv2.Sobel(gaussianBluredImage,cv2.CV_64F,0,1,ksize=3)
 
 #apply canny algorithm which includes all the previos steps
-edges = cv2.Canny(gaussianBluredImage, 50, 300, 19)
-#find contours
-contours, hierarchy = cv2.findContours(edges,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-for contourNr in range(len(contours)):
-    cv2.drawContours(resultImage, contours, contourNr, (random.randint(0, 255),random.randint(0, 255),random.randint(0, 255)), 2)
+edges = cv2.Canny(gaussianBluredImage, 0, 200, 3)
 
+#find contours
+contours, hierarchy = cv2.findContours(edges,cv2.RETR_TREE,cv2.CHAIN_APPROX_NONE)
+
+
+#get the bounding rectangles areas of the contours and insert it into an array of tuples tha contain the contour and the rectangle bounding area
+boundingRectanglesAreas = []
+for contour in contours:
+    rect = cv2.minAreaRect(contour)
+    points = cv2.boxPoints(rect)
+    points = np.int0(points)
+    area = np.sqrt((points[0][0] - points[1][0]) ** 2 + (points[0][1] - points[1][1]) ** 2) * np.sqrt((points[1][0] - points[2][0]) ** 2 + (points[1][1] - points[2][1]) ** 2)
+    boundingRectanglesAreas.append((contour, area))
+
+#find the maximum rectangle bounding area of the contours and create a threshold
+boundingAreaExclusionThreshold = np.max(np.array([boundingRectanglesArea[1] for boundingRectanglesArea in boundingRectanglesAreas])) / 2000
+
+#get only the contours which bounding rectangles have a area smaller than a certain threshold so that we can eliminate the text from the image
+#check if the solidity of the contour is big enough(checking the convexity of the contour by computing the ratio of the contour area to the bounding region area)
+contoursWithoutText = []
+for boundingRectangleArea in boundingRectanglesAreas:
+    if ~cv2.isContourConvex(boundingRectangleArea[0]) and boundingRectangleArea[1] > boundingAreaExclusionThreshold:
+        contoursWithoutText.append(boundingRectangleArea[0])
+
+#draw contours
+for contourNr in range(len(contoursWithoutText)):
+    cv2.drawContours(resultImage, contoursWithoutText, contourNr, (random.randint(0, 255),random.randint(0, 255),random.randint(0, 255)), 2)
 
 #show processed photo near the original grayscale photo
 plt.figure(1)
 plt.subplot(1, 2, 1)
-plt.title("Grayscale image")
-plt.imshow(grayScaleImage, cmap="gray", vmin=0, vmax=255)
-plt.subplot(1, 2, 2)
 plt.title("Edges")
+plt.imshow(edges, cmap="gray", vmin=0, vmax=255)
+plt.subplot(1, 2, 2)
+plt.title("Contours")
 plt.imshow(resultImage, cmap="gray", vmin=0, vmax=255)
 plt.show()
