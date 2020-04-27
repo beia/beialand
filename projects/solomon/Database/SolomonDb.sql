6649f31c366ae84d1a1332645670c37f3ccd8fec
cristianBalanean <cristianbalanean_etti@yahoo.com>
2020-04-27 10:59:56 +0300

    [SOLOMON]: database update

--- projects/solomon/Database/SolomonDb.sql
+++ projects/solomon/Database/SolomonDb.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `solomondb` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `solomondb`;
 -- MySQL dump 10.13  Distrib 8.0.19, for Win64 (x86_64)
 --
 -- Host: localhost    Database: solomondb
@@ -392,4 +394,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-04-27 10:53:51
+-- Dump completed on 2020-04-27 10:59:25
