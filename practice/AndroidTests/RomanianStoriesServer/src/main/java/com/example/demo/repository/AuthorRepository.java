e98fb40fd80309e02e207ee6068a971f93c2245f
Balanean Cristian <cristianbalanean_etti@yahoo.com>
2020-12-07 18:34:22 +0200

    [ANDROID_TESTS]: MVVM Android design pattern, tested the retrofit2 rest client library for Android, created a rest server that provides books

--- practice/AndroidTests/RomanianStoriesServer/src/main/java/com/example/demo/repository/AuthorRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.model.Author;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AuthorRepository extends JpaRepository<Author, Long> {
-}
--- practice/AndroidTests/RomanianStoriesServer/src/main/java/com/example/demo/repository/StoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.model.Story;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StoryRepository extends JpaRepository<Story, Long> {
-}
